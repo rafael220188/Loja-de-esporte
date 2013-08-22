c2ace4030d2f49b57f72ed851e6f0e2938f0fa70
rafael220188 <rafaelsmdm@gmail.com>
2013-08-22 09:29:40 -0300

    atualização

--- ProjetoLab03/src/JDBCteste.java
+++ ProjetoLab03/src/JDBCteste.java
@@ -1,14 +1,24 @@
-
-import java.sql.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class JDBCteste {
 
-	public static void main(String[] args) throws SQLException {
-		String url = "jdbc:postgresql://localhost:5432/Loja";
-		String username = "postgres";
-		String password = "1234";
+	// String url = "jdbc:postgresql://localhost:5432/Loja";
+	// String username = "postgres";
+	// String password = "1234";
 
-		Connection db = DriverManager.getConnection(url, username, password);
+	// http://jdbc.postgresql.org/documentation/head/connect.html
+	public static void main(String[] args) throws SQLException, FileNotFoundException, IOException {
+		Properties props = new Properties();
+		props.load(new FileInputStream("lojaEsporte.properties"));
+		String url = props.getProperty("url");
+
+		Connection db = DriverManager.getConnection(url, props);
 		System.out.println("Conectado!");
 	}
 }
